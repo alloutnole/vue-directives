d35441d8575b55dccb356566d3b7165bac2910df
Ken Pace <ken.pace@spathesystems.com>
2021-07-20 22:35:08 -0400

    Format typography and add headings

--- src/components/Directives.vue
+++ src/components/Directives.vue
@@ -1,41 +1,47 @@
 <template>
     <div id="directives" class="has-background-light">
         <div class="columns is-desktop">
-            <div class="column">     
-                <div class="content">Message: <b>{{message}}</b></div> 
+            <div class="column">
+                <h2 class="subtitle is-5">v-text</h2>    
+                <div class="content">Message: <b>{{message}}</b></div>
+                <h2 class="subtitle is-5">v-html</h2> 
                 <div class="content" v-html="sampleHTML"></div>
+                <h2 class="subtitle is-5">v-show</h2>
                 <div class="content" v-show="isDisplay"><b> ... v-show = isDisplay ... </b></div>
+                <h2 class="subtitle is-5">v-if / v-else</h2>
                 <div class="content" v-if="2 > 1"><b> ... v-if = 2 > 1 ... </b></div>
                 <div class="content">
                     <b-button @click="toggleIsRender" type="is-primary">Toggle isRender</b-button>
                 </div> 
                 <div class="content" v-if="isRender"><b>isRender = true</b></div>
-                <div class="content" v-else><b>else isRender = false</b></div>        
-                <div class="content">
-                    <h2 class="subtitle is-4">v-for over an array of numbers 0 - 9</h2>
+                <div class="content" v-else><b>else isRender = false</b></div> 
+                <h2 class="subtitle is-5">v-for</h2>       
+                <div class="content">                    
                     <p><span>{{ rawHTML }}</span></p>
                     <ul>
                         <li v-for="number in numbers" :key="number">{{ number }}</li>
                     </ul>
                 </div>
+                <h2 class="subtitle is-5">:src</h2>
                 <div class="content">
                     <img :src="imageSrc" width="50%" />
                 </div>
-                <div class="content">
+                <h2 class="subtitle is-5">v-model</h2>
+                <div class="content">                    
                     <b-field label="Message:">
                         <b-input v-model="message"/>
                     </b-field>
-                    <p>Message is: <b>{{ message }}</b></p> 
+                    <p>Message is: {{ message }}</p> 
                 </div>           
                 <div class="content">
                     <b-field label="Multi-line Textarea:">
                         <b-input type="textarea" v-model="multilineText"></b-input>
                     </b-field>
-                    <b style="white-space: pre-line;">{{ multilineText }}</b>
+                    <p style="white-space: pre-line;">{{ multilineText }}</p>
                 </div>
                 <div class="content">
                     <b-checkbox id="checkbox" v-model="checked">
-                        <b>{{ checked }}</b>
+                        {{ checked }}
                     </b-checkbox>
                 </div>
                 <div class="content">
@@ -43,7 +49,7 @@
                     <b-radio v-model="picked" name="number" native-value="Two">Two</b-radio>
                     <b-radio v-model="picked" name="number" native-value="Three">Three</b-radio>
                     <p class="content">
-                        <b>Picked: {{ picked }}</b>
+                        Picked: {{ picked }}
                     </p>
                 </div>
                 <div class="content">
@@ -54,7 +60,7 @@
                             </option>
                         </b-select>
                     </b-field>
-                    <b>Selected: {{ selected }}</b>
+                    <p>Selected: {{ selected }}</p>
                 </div>
             </div>
         </div>
@@ -72,8 +78,8 @@ export default {
             message: 'Default Message',
             multilineText: 'Sample Text',
             checked: false,
-            sampleHTML: '<ul><li>One</li><li>Two</li><li>Three</li></ul>',
-            rawHTML: '<ul><li v-for="number in numbers" :key="number">{{ number }}</li></ul>',
+            sampleHTML: '<ol> <li>One</li> <li>Two</li> <li>Three</li> </ol>',
+            rawHTML: '<ul> <li v-for="number in numbers" :key="number">{{ number }}</li> </ul>',
             isDisplay: true,
             isRender: true,
             numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
     <div>
         <h2 class="title">Home</h2>
-        <collapse />
         <directives />
     </div>
 </template>
 
 <script>
-import Collapse from '@/components/Collapse.vue'
 import Directives from '@/components/Directives.vue'
 
 export default {
     name: 'Home',
     components: {
-        Collapse,
         Directives,
     },
     data() {
